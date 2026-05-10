5577dc3b9ab5f7108cb18dc8efa9f3faebb6165d
CoderRushikesh <rushikamble462@gmail.com>
2026-05-10 23:04:09 +0530

    code the httpEngine - to call the other apis , handle the exception in order to call the api

---
+++ src/main/java/com/paypal/pojo/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.paypal.pojo;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+	
+	private String errorCode;
+	private String errorMessage;
+	
+	
+	
+	
+	
+}
